737a8aa10cf2747a09d54eb4ac88a76a12854d49
unknown <thelinh2711@gmail.com>
2024-12-13 21:15:57 +0700

    abc

--- src/main/java/UDP/nkAo3RTY.java
+++ src/main/java/UDP/nkAo3RTY.java
@@ -1,4 +1,16 @@
 package UDP;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class nkAo3RTY {
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress serverAddress = InetAddress.getByName("203.162.10.109");
+        int serverPort = 2207;
+    }
+
 }
+g
