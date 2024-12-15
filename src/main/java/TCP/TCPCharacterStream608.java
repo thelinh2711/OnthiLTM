538dec062e5550dff7d7777a075ada37e7143615
unknown <thelinh2711@gmail.com>
2024-12-16 00:49:47 +0700

    update

--- src/main/java/TCP/TCPCharacterStream608.java
+++ src/main/java/TCP/TCPCharacterStream608.java
@@ -2,6 +2,10 @@ package TCP;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class TCPCharacterStream608 {
     public static void main(String[] args) throws IOException {
@@ -9,11 +13,20 @@ public class TCPCharacterStream608 {
         BufferedWriter dos = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         BufferedReader dis = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        dos.write("B21DCCN492;IiWePAlo");
-        dos.flush();
+        dos.write("B21DCCN608;Fg1elzfe");
         dos.newLine();
+        dos.flush();
 
         String s = dis.readLine();
         System.out.println(s);
+        String[] a = s.split(" ");
+        List<String> list = new ArrayList<>(Arrays.asList(a));
+        Collections.sort(list);
+        String ans = String.join(" ", list);
+        dos.write(ans);
+        dos.newLine();
+        dos.flush();
+
+        socket.close();
     }
 }
