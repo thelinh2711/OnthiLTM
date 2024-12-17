fcf5fa4f70e5fa0e052b9f784f6b9dcdf137a288
unknown <thelinh2711@gmail.com>
2024-12-17 23:35:00 +0700

    fix rmiCharater

--- src/main/java/RMI/RMICharacter486.java
+++ src/main/java/RMI/RMICharacter486.java
@@ -2,13 +2,14 @@ package RMI;
 
 import java.net.MalformedURLException;
 import java.rmi.*;
-import java.sql.SQLOutput;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Base64;
 
 public class RMICharacter486 {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
-        String urlRmi = "rmi://203.162.10.109/RMICharacterService";
-        CharacterService service = (CharacterService) Naming.lookup(urlRmi);
+        Registry registry = LocateRegistry.getRegistry("203.162.10.109");
+        CharacterService service = (CharacterService) registry.lookup("RMICharacterService");
 
         String inputStr = service.requestCharacter("B21DCCN486", "v9Hie5vH");
         System.out.println(inputStr);
